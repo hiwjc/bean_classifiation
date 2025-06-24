820fb3840ad881d307e159be7edeb5586fddb6a4
hiwjc <hiwjc@naver.com>
2025-06-25 00:37:23 +0900

    Update bean_train.py

--- bean_train.py
+++ bean_train.py
@@ -1,19 +1,5 @@
 # train
 
-
-
-
-import torch
-
-def collate_fn(batch):
-    return {
-        'pixel_values': torch.stack([x['pixel_values'] for x in batch]),
-        'labels': torch.tensor([x['labels'] for x in batch])
-    }
-
-
-
-
 from transformers import Trainer
 
 trainer = Trainer(
